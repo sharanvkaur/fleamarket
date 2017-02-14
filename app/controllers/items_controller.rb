e3b1663b7b39d5d0802d23484449653c130d1c2c
Esther Low <phi21416@gmail.com>
2017-02-14 10:03:30 +0800

    added create and show items controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+
   def new
   end
-end
+
+  def create
+    @user = current_user
+    @item = @user.items.create(item_params)
+    redirect_to user_path
+  end
+
+  def show
+    @user = current_user
+    @items = @user.items
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category, :price, :condition, :description, :stock, :photo)
+  end
+
+  end
