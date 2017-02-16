95ee907371ab33d305b3d500116938086ea0afde
Sharan Kaur <sharanvkaur@gmail.com>
2017-02-17 03:55:30 +0800

    wat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
+    current_user = User.find_by_id(params[:id])
+    @events = Event.where("user_id = ?", current_user.id)
     render :show
-
   end
 end
